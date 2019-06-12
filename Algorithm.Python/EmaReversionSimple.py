d23bbd514f566820507cb6730eda2fa23c9d35c0
franklili <348104201@qq.com>
2019-06-12 19:35:49 +0800

    Margin trade framework for VXX

--- Algorithm.Python/EmaReversionSimple.py
+++ Algorithm.Python/EmaReversionSimple.py
@@ -11,56 +11,70 @@ from QuantConnect import Resolution, Extensions
 from QuantConnect.Algorithm import *
 
 import statistics
+from QuantConnect.Algorithm.Framework.Risk import *
+from QuantConnect.Algorithm.Framework.Execution import VolumeWeightedAveragePriceExecutionModel
+from Portfolio.EqualWeightingPortfolioConstructionModel import EqualWeightingPortfolioConstructionModel
+from QuantConnect.Algorithm.Framework.Alphas import *
+
 class EmaReversionAlgorithm(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2018, 6, 1)  # Set Start Date
+        self.SetStartDate(2018, 1, 15)  # Set Start Date
         self.SetCash(100000)  # Set Strategy Cash
         self.AddEquity("SPY")
         self.symbol = 'VXX'
         self.AddEquity(self.symbol, Resolution.Minute).Symbol
-        self.span = 1050
-        self.ema = self.EMA(self.symbol, self.span)
-        self.longPeriod = 10080
-        self.PositiveAbnormalIsOverDiff_pct_close_ema = False
-        self.Diff_pct_close_emaIsOverNegativeAbnormal = False 
-        self.diff_pct_close_ema = 0
-        self.diff_pct_close_ema_sum = 0
-        self.longPeriod_index = 0
-        self.diff_pct_close_ema_list = []
-        self.diff_pct_close_ema_mean = 0
-        self.diff_pct_close_ema_std = 0
-        self.negative_abnormal = 0
-        self.positive_abnormal = 0
-    
-    def OnData(self, data):
-        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
-            Arguments:
-                data: Slice object keyed by symbol containing the stock data
-        '''
+        self.AddAlpha(EmaReversionAlphaModel(self.symbol))
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
+        self.SetRiskManagement(MaximumDrawdownPercentPortfolio(0.1))
+        self.SetExecution(VolumeWeightedAveragePriceExecutionModel())
+
+class EmaReversionAlphaModel(AlphaModel):
+    def __init__(self, symbol):
+        self.symbol = symbol
+        
+    def Update(self, algorithm, data):
+        insights = []
+        span = 1050
+        longPeriod = 5000
+        PositiveAbnormalIsOverDiff_pct_close_ema = False
+        Diff_pct_close_emaIsOverNegativeAbnormal = False 
+        diff_pct_close_ema_sum = 0
+        longPeriod_index = 0
+        diff_pct_close_ema_list = []
+        diff_pct_close_ema_mean = 0
+        diff_pct_close_ema_std = 0
+        negative_abnormal = 0
+        positive_abnormal = 0
+        diff_pct_close_ema = 0
+        ema = algorithm.EMA(self.symbol, span)
+        
         if not data.ContainsKey(self.symbol): return
         if not data.ContainsKey('SPY'): return
     
-        self.diff_pct_close_ema = (self.Securities[self.symbol].Price - self.ema.Current.Value) / self.ema.Current.Value
-        self.longPeriod_index += 1
-        self.diff_pct_close_ema_sum += self.diff_pct_close_ema
-        self.diff_pct_close_ema_list.append(self.diff_pct_close_ema)
+        diff_pct_close_ema = (self.Securities[self.symbol].Price - ema.Current.Value) / ema.Current.Value
+        longPeriod_index += 1
+        diff_pct_close_ema_sum += diff_pct_close_ema
+        diff_pct_close_ema_list.append(diff_pct_close_ema)
             
-        if self.longPeriod_index == self.longPeriod:
-            self.diff_pct_close_ema_mean = self.diff_pct_close_ema_sum / self.longPeriod_index
-            self.diff_pct_close_ema_std = statistics.stdev(self.diff_pct_close_ema_list)
-            self.negative_abnormal = self.diff_pct_close_ema_mean - 3 * self.diff_pct_close_ema_std
-            self.positive_abnormal = self.diff_pct_close_ema_mean + 3 * self.diff_pct_close_ema_std
-            self.longPeriod_index = 0
-            self.diff_pct_close_ema_sum = 0
-            self.diff_pct_close_ema_list = []
-        if self.Portfolio[self.symbol].IsLong and self.PositiveAbnormalIsOverDiff_pct_close_ema and (self.diff_pct_close_ema > self.positive_abnormal):
-            self.SetHoldings(self.symbol, -1, True, 'Short')
-        elif ((not self.Portfolio.Invested) or (self.Portfolio[self.symbol].IsShort)) and self.Diff_pct_close_emaIsOverNegativeAbnormal and (self.diff_pct_close_ema < self.negative_abnormal):
-            self.SetHoldings(self.symbol, 1, True, 'Long')
-        self.PositiveAbnormalIsOverDiff_pct_close_ema = self.positive_abnormal > self.diff_pct_close_ema
-        self.Diff_pct_close_emaIsOverNegativeAbnormal = self.diff_pct_close_ema > self.negative_abnormal
-       
-        self.Plot('Deviation', 'VXX', self.Securities[self.symbol].Price)
-        self.Plot('Deviation', 'EMA', self.ema.Current.Value)
-        self.Plot('Deviation Degree', 'diff_pct_close_vs_ema', self.diff_pct_close_ema)
+        if longPeriod_index == longPeriod:
+            diff_pct_close_ema_mean = diff_pct_close_ema_sum / longPeriod_index
+            diff_pct_close_ema_std = statistics.stdev(diff_pct_close_ema_list)
+            negative_abnormal = diff_pct_close_ema_mean - 3 * diff_pct_close_ema_std
+            positive_abnormal = diff_pct_close_ema_mean + 3 * diff_pct_close_ema_std
+            longPeriod_index = 0
+            diff_pct_close_ema_sum = 0
+            diff_pct_close_ema_list = []
+        if self.Portfolio[self.symbol].IsLong and PositiveAbnormalIsOverDiff_pct_close_ema and (diff_pct_close_ema > positive_abnormal):
+            insights.append(Insight.Price(self.symbol, Resolution.Minute, 1, InsightDirection.Down))
+            #self.SetHoldings(self.symbol, -1, True, 'Short')
+        elif ((not self.Portfolio.Invested) or (self.Portfolio[self.symbol].IsShort)) and Diff_pct_close_emaIsOverNegativeAbnormal and (diff_pct_close_ema < negative_abnormal):
+            insights.append(Insight.Price(self.symbol, Resolution.Minute, 1, InsightDirection.Up))
+            #self.SetHoldings(self.symbol, 1, True, 'Long')
+        PositiveAbnormalIsOverDiff_pct_close_ema = positive_abnormal > diff_pct_close_ema
+        Diff_pct_close_emaIsOverNegativeAbnormal = diff_pct_close_ema > negative_abnormal
+        if (diff_pct_close_ema > positive_abnormal) or (diff_pct_close_ema < negative_abnormal):
+            self.Plot('Deviation', 'VXX', self.Securities[self.symbol].Price)
+            self.Plot('Deviation', 'EMA', ema.Current.Value)
+            self.Plot('Deviation Degree', 'diff_pct_close_vs_ema', diff_pct_close_ema)
+        return insights
